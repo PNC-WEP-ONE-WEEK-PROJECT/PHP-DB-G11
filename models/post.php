20fe8bcc62caa83aada36b4c2771d474cacf6a63
KHY.PHAT <khy.phat@PNC.LAN>
2022-03-16 14:22:41 +0700

    add data from user input text into database

--- models/post.php
+++ models/post.php
@@ -1,4 +1,20 @@
 <?php
-/**
- * Your code here
- */
+    require_once('database.php');
+   
+    // Add post
+    $description = $_POST['description'];
+    
+    function createPost($description)
+    {
+        global $db;
+        $statement = $db -> prepare("INSERT INTO posts(postDate, description) values (now(), :description );");
+        $statement -> execute([
+            ':description' => $description,
+    ]);
+    return $statement;
+}
+// if ($description != null ){
+    createPost($description);
+// }
+header('location: add_post.php');
+?>
--- templates/header.php
+++ templates/header.php
@@ -5,7 +5,26 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Facebook 2.0</title>
-    <!-- Your style here -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <link rel="stylesheet" href="../css/style.css">
 </head>
 <body>
-    
+    <!-- NAVIGATION -->
+<section class="display-size w-50 m-auto bg-light">
+    <nav id="navigation" class="navbar py-0 bg-light">
+        <a class="navbar-brand px-3" href="#"><img class="logo" src="images/logo.png" alt=""></a>
+        <ul class="nav nav-pills d-flex justify-content-between menu">
+            <li class="nav-item">
+                <a class="nav-link" href=""><i class="material-icons menu-icon">home</i></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href=""><i class="material-icons menu-icon">people</i></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href=""><i class="material-icons menu-icon">account_circle</i></a>
+            </li>
+        </ul>
+    </nav>
+    <div class="container">
